7ed26219effd4d0eeb6c6b5d99eb3ca14415e5a2
Pedro Henrique <pedrosilveira07@outlook.com>
2021-12-08 02:32:28 -0300

    feat(index): added types to redux and init Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import { Home } from './screens';
+import store from './store';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <Provider store={store}>
       <Home />
       <GlobalStyle />
-    </>
+    </Provider>
   );
 };
 
