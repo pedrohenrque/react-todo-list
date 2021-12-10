bc608e260e6fabc1443941e0d28d993870d126c1
Pedro Henrique <pedrosilveira07@outlook.com>
2021-12-10 10:35:22 -0300

    refactor(routes): refactored route base

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
+import Routes from './routes/routes';
 import { Home } from './screens';
 import { store } from './store';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Home />
-      <GlobalStyle />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes />
+        <GlobalStyle />
+      </Provider>
+    </BrowserRouter>
   );
 };
 
